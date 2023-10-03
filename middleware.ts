33f1a9827c530b760900d1e93b7a6f774f514e15
Carl Godlewski <cgodlew@gmail.com>
2023-10-03 13:20:13 -0400

    Added Logic to redirect logged in users away from public routes. Adjust casing to avoid compiling errors for sidebar component.

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,25 @@
-import { authMiddleware } from "@clerk/nextjs";
- 
+import { authMiddleware } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
+
+
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
+
 export default authMiddleware({
-    publicRoutes: ["/"]
+	publicRoutes: ['/'],
+	afterAuth(auth, req, evt) {
+
+        //redirect logged in users away from the landing page
+		if (auth.userId && auth.isPublicRoute) {
+            const redirectToDash = new URL('/dashboard', req.url)
+            return NextResponse.redirect(redirectToDash)
+        }
+
+			
+	},
 });
- 
+
 export const config = {
-      matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+	matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
- 
